1d2898e9ab75fd95231d32942cbe027b40774ea6
Josh Coles <josh@joshcoles.com>
2019-07-07 22:02:00 -0700

    Begin building out app.

--- gshd/src/App.js
+++ gshd/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './styles/app.scss';
+import { tsConstructorType } from '@babel/types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    articleLink: 'https://www.reddit.com/r/hockey/comments/3e518u/wayne_gretzkys_strange_eating_habits/'
+  }
+
+  render() {
+    return (
+      <div id="gshd" className="App">
+        <header>
+        <nav></nav>
+          <h1>Gretzky-Style Hot Dog</h1>
+          <span>An ode to Waynge Gretzky's strange eating habits.</span>
+        </header>
+        <section>
+          <h2>What is this?</h2>
+          <p>On June 15th, someone created <a href={this.state.articleLink}>this post</a> on the /r/hockey subreddit. That user has since deleted their account, but their story will live on forever.</p>
+          <p></p>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
